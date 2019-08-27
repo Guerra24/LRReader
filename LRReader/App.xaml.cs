a6c3dde7c961c086f215128a8559a1872706c3ab
Guerra24 <pablo230699@hotmail.com>
2019-08-27 13:28:29 -0600

    Removed experimental tabbed host

--- LRReader/App.xaml.cs
+++ LRReader/App.xaml.cs
@@ -68,7 +68,7 @@ namespace LRReader
                     // When the navigation stack isn't restored navigate to the first page,
                     // configuring the new page by passing required information as a navigation
                     // parameter
-                    rootFrame.Navigate(typeof(HostTabPage), e.Arguments);
+                    rootFrame.Navigate(typeof(HostPage), e.Arguments);
                 }
                 // Ensure the current window is active
                 Window.Current.Activate();
