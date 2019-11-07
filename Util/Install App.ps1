f6eae760fade69077399819d3da51ec91cafd8b1
Guerra24 <pablo230699@hotmail.com>
2019-11-07 11:45:46 -0600

    Custom install script

---
+++ Util/Install App.ps1
@@ -0,0 +1,16 @@
+﻿param(
+    [switch]$Force = $false
+)
+
+$scriptArgs = ""
+if ($Force)
+{
+    $scriptArgs = '-Force'
+}
+
+$currLocation = Get-Location
+Set-Location $PSScriptRoot
+Set-Location $(Get-ChildItem *LRReader* -Directory)
+Invoke-Expression ".\Install.ps1 $scriptArgs"
+CheckNetIsolation "loopbackexempt" "-a" "-n=Guerra24.LRReader_3fr0p4qst6948"
+Set-Location $currLocation
