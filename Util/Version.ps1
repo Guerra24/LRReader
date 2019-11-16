1847875099c59c938a29c64e85c94bc1ba9e387c
Guerra24 <pablo230699@hotmail.com>
2019-11-16 14:20:03 -0600

    Test generating app version on runtime for CD packages

---
+++ Util/Version.ps1
@@ -0,0 +1,11 @@
+﻿$xmlFileName = "LRReader.UWP\Package.appxmanifest"      
+[xml]$xmlDoc = Get-Content $xmlFileName
+
+$count = "C:\Program Files\Git\mingw64\bin\git.exe" rev-list --all --count
+$version = $xmlDoc.Package.Identity.Version;
+$substr = "."
+$xmlDoc.Package.Identity.Version = $version.Remove(($lastIndex = $version.LastIndexOf($substr) + 1)).Insert($lastIndex,$count)
+
+echo "Version:" $xmlDoc.Package.Identity.Version
+
+$xmlDoc.Save("$(Get-Location)/$xmlFileName")
