9b84e1c94c4217b8907fa24cf83d6b3e440ad535
Guerra24 <pablo230699@hotmail.com>
2020-05-22 23:17:33 -0600

    Switch back version scheme

--- Util/Version.ps1
+++ Util/Version.ps1
@@ -4,7 +4,7 @@
 $count = & "C:\Program Files\Git\mingw64\bin\git.exe" rev-list --count HEAD
 $version = [System.Version]::Parse($xmlDoc.Package.Identity.Version);
 
-$Field = $version.GetType().GetField('_Build','static,nonpublic,instance')
+$Field = $version.GetType().GetField('_Revision','static,nonpublic,instance')
 $Field.SetValue($version,$count -as [int])
 
 $xmlDoc.Package.Identity.Version = $version.ToString();
