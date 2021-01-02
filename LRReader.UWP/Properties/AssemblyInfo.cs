e74e6465acc049a8d9a1b5abc22e76855cda3fc5
Guerra24 <pablo230699@hotmail.com>
2021-01-01 21:38:52 -0600

    Will it build?

--- LRReader.UWP/Properties/AssemblyInfo.cs
+++ LRReader.UWP/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("LRReader")]
-[assembly: AssemblyCopyright("Copyright © Guerra24 2019")]
+[assembly: AssemblyCopyright("Copyright © Guerra24 2019-2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
