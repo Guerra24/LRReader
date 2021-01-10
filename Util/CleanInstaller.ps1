20a0d0f15421cc03f757fd210556f747348189d0
Guerra24 <pablo230699@hotmail.com>
2021-01-09 20:05:14 -0600

    Build optimizations

---
+++ Util/CleanInstaller.ps1
@@ -0,0 +1,17 @@
+﻿$xmlFileName = "./LRReader.UWP/AppPackages/LRReader.UWP.appinstaller"      
+[xml]$xmlDoc = Get-Content $xmlFileName
+
+$node = $xmlDoc.AppInstaller.UpdateSettings
+$xmlDoc.AppInstaller.RemoveChild($node) | Out-Null
+
+$xwSettings = new-object System.Xml.XmlWriterSettings
+$xwSettings.Indent = $true
+$xwSettings.IndentChars = "`t"
+$xwSettings.NewLineOnAttributes = $true
+
+$xmlWriter = [Xml.XmlWriter]::Create("$(Get-Location)/$xmlFileName", $xwSettings)
+
+$xmlDoc.Save($xmlWriter)
+
+$xmlWriter.Flush()
+$xmlWriter.Close()
