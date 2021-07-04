5865c86d3b200a7c5917ba180caba76cf725a499
Guerra24 <pablo230699@hotmail.com>
2021-07-03 21:49:50 -0600

    Redesign reader settings
    Follows Win11's card pattern.
    Created two new controls.
    ModernInput for wrapping single controls.
    ModernExpander for grouping controls.

---
+++ LRReader.UWP/Views/Controls/ModernExpander.xaml.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.UI.Xaml.Controls;
+using System.Collections.Generic;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace LRReader.UWP.Views.Controls
+{
+	public sealed partial class ModernExpander : Expander
+	{
+		public ModernExpander()
+		{
+			this.InitializeComponent();
+			Items = new List<object>();
+		}
+
+		public object Input
+		{
+			get => GetValue(InputProperty);
+			set => SetValue(InputProperty, value);
+		}
+
+		public string HeaderText
+		{
+			get => GetValue(HeaderTextProperty) as string;
+			set => SetValue(HeaderTextProperty, value);
+		}
+		public string SubText
+		{
+			get => GetValue(SubTextProperty) as string;
+			set => SetValue(SubTextProperty, value);
+		}
+
+		public IList<object> Items
+		{
+			get => GetValue(ItemsProperty) as IList<object>;
+			set => SetValue(ItemsProperty, value);
+		}
+
+		public static readonly DependencyProperty InputProperty = DependencyProperty.Register("Input", typeof(object), typeof(ModernExpander), new PropertyMetadata(null));
+		public static readonly DependencyProperty HeaderTextProperty = DependencyProperty.Register("HeaderText", typeof(string), typeof(ModernExpander), new PropertyMetadata(""));
+		public static readonly DependencyProperty SubTextProperty = DependencyProperty.Register("SubText", typeof(string), typeof(ModernExpander), new PropertyMetadata(""));
+		public static readonly DependencyProperty ItemsProperty = DependencyProperty.Register("Items", typeof(IList<object>), typeof(ModernExpander), new PropertyMetadata(new List<object>()));
+	}
+
+	public class ItemTemplateSelector : DataTemplateSelector
+	{
+		public DataTemplate LastItem { get; set; }
+		public DataTemplate OtherItem { get; set; }
+
+		protected override DataTemplate SelectTemplateCore(object item, DependencyObject container)
+		{
+			var itemsControl = ItemsControl.ItemsControlFromItemContainer(container);
+			return (itemsControl.IndexFromContainer(container) == (itemsControl.ItemsSource as IList<object>).Count - 1) ? LastItem : OtherItem;
+		}
+	}
+}
---
+++ LRReader.UWP/Views/Controls/ModernInput.xaml.cs
@@ -0,0 +1,41 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace LRReader.UWP.Views.Controls
+{
+	public sealed partial class ModernInput : ContentControl
+	{
+		public ModernInput()
+		{
+			this.InitializeComponent();
+		}
+
+		public new object Content
+		{
+			get => GetValue(ContentProperty);
+			set => SetValue(ContentProperty, value);
+		}
+
+		public string HeaderText
+		{
+			get => GetValue(HeaderTextProperty) as string;
+			set => SetValue(HeaderTextProperty, value);
+		}
+		public string SubText
+		{
+			get => GetValue(SubTextProperty) as string;
+			set => SetValue(SubTextProperty, value);
+		}
+
+		public HorizontalAlignment InputHorizontalAlignment
+		{
+			get => (HorizontalAlignment)GetValue(InputHorizontalAlignmentProperty);
+			set => SetValue(InputHorizontalAlignmentProperty, value);
+		}
+
+		public static readonly new DependencyProperty ContentProperty = DependencyProperty.Register("Content", typeof(object), typeof(ModernInput), new PropertyMetadata(null));
+		public static readonly DependencyProperty HeaderTextProperty = DependencyProperty.Register("HeaderText", typeof(string), typeof(ModernInput), new PropertyMetadata(""));
+		public static readonly DependencyProperty SubTextProperty = DependencyProperty.Register("SubText", typeof(string), typeof(ModernInput), new PropertyMetadata(""));
+		public static readonly DependencyProperty InputHorizontalAlignmentProperty = DependencyProperty.Register("InputHorizontalAlignment", typeof(HorizontalAlignment), typeof(ModernInput), new PropertyMetadata(HorizontalAlignment.Right));
+	}
+}
