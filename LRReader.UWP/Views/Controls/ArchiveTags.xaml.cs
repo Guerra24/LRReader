dd480fac48cc940a5227bd7f3353f5cf56f02577
Guerra24 <pablo230699@hotmail.com>
2021-09-27 19:11:25 -0600

    The absolute state of UWP Bindings

--- LRReader.UWP/Views/Controls/ArchiveTags.xaml.cs
+++ LRReader.UWP/Views/Controls/ArchiveTags.xaml.cs
@@ -40,8 +40,24 @@ namespace LRReader.UWP.Views.Controls
 			set => SetValue(ItemsSourceProperty, value);
 		}
 
+		public double MaxTagsWidth
+		{
+			get => (double)GetValue(MaxTagsWidthProperty);
+			set => SetValue(MaxTagsWidthProperty, value);
+		}
+
 		public static readonly DependencyProperty ItemsSourceProperty = DependencyProperty.Register("ItemsSource", typeof(ICollection<ArchiveTagsGroup>), typeof(ArchiveTags), new PropertyMetadata(new List<ArchiveTagsGroup>()));
 		public static readonly DependencyProperty ItemClickCommandProperty = DependencyProperty.Register("ItemClickCommand", typeof(ICommand), typeof(ArchiveTags), new PropertyMetadata(null));
+		public static readonly DependencyProperty MaxTagsWidthProperty = DependencyProperty.Register("MaxTagsWidth", typeof(double), typeof(ArchiveTags), new PropertyMetadata(double.MaxValue));
 
+		private void TagsList_Loaded(object sender, RoutedEventArgs e)
+		{
+			var grid = sender as GridView;
+			if (grid.Tag == null)
+			{
+				grid.MaxWidth = MaxTagsWidth;
+				grid.Tag = "The absolute state of UWP Bindings";
+			}
+		}
 	}
 }
