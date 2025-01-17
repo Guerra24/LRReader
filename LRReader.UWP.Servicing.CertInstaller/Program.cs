056ea327c55321e5e16ccb2a113c4a6f4e0e2fda
Guerra24 <lili@guerra24.net>
2025-01-17 02:36:19 -0600

    Workaround deadlock on arm64

--- LRReader.UWP.Servicing.CertInstaller/Program.cs
+++ LRReader.UWP.Servicing.CertInstaller/Program.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using System.Diagnostics;
 using System.IO;
 using System.Net.Http;
 using System.Reflection;
 using System.Security.Cryptography.X509Certificates;
 using System.Security.Principal;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace LRReader.UWP.Servicing.CertInstaller;
@@ -13,9 +15,21 @@ internal static class Program
 	static async Task Main(string[] args)
 	{
 #if DEBUG
-		if (!System.Diagnostics.Debugger.IsAttached)
-			System.Diagnostics.Debugger.Launch();
+		if (!Debugger.IsAttached)
+			Debugger.Launch();
 #endif
+		var killer = new Thread(() =>
+		{
+			try
+			{
+				// Wait 10 seconds before killing process
+				// We can randomly deadlock on some systems (arm64 for some mysterious reason) so make sure we don't keep a cpu thread at 100% usage
+				Thread.Sleep(10000);
+				Process.GetCurrentProcess().Kill();
+			}
+			catch (ThreadInterruptedException) { }
+		});
+		killer.Start();
 		using var identity = WindowsIdentity.GetCurrent();
 		var principal = new WindowsPrincipal(identity);
 		if (!principal.IsInRole(WindowsBuiltInRole.Administrator))
@@ -29,6 +43,7 @@ internal static class Program
 			var res = await ProcessUtil.LaunchAdmin(Assembly.GetExecutingAssembly().Location, file);
 			if (res == -99)
 				File.Delete(file);
+			killer.Interrupt();
 			return;
 		}
 		if (File.Exists(args[0]))
@@ -39,5 +54,6 @@ internal static class Program
 			CertUtil.Close();
 			File.Delete(args[0]);
 		}
+		killer.Interrupt();
 	}
 }
