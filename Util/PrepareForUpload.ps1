aac862b338779664174a9484fe6c2e42c8654404
Guerra24 <lili@guerra24.net>
2025-02-27 14:17:09 -0600

    Remove unused items before uploading

--- Util/PrepareForUpload.ps1
+++ Util/PrepareForUpload.ps1
@@ -1,18 +1,7 @@
-﻿[xml]$xmlDoc = Get-Content "./LRReader.UWP/Package.appxmanifest"
-
-Set-Location "./LRReader.UWP/AppPackages/"
-# Rename-Item $(Get-ChildItem *LRReader* -Directory) "$(Get-Location)/LRReader.UWP"
-
-$installer = "$(Get-Location)/LRReader.UWP.appinstaller"
-$html = "$(Get-Location)/index.html"
-$text = "_$($xmlDoc.Package.Identity.Version)_Test"
-
-$content = [System.IO.File]::ReadAllText($installer).Replace($text, "")
-[System.IO.File]::WriteAllText($installer, $content)
-
+﻿Set-Location "./LRReader.UWP/AppPackages/"
+Remove-Item -Path "./index.html"
 Set-Location "./LRReader.UWP"
-Remove-Item -Path $html
-# Remove-Item -Path "./Install.ps1","./Add-AppDevPackage.ps1"
-# Remove-Item -Path "./Add-AppDevPackage.resources" –recurse
+Get-ChildItem "*.cer" | foreach { Remove-Item -Path $_.FullName }
+Get-ChildItem "*.msix" | foreach { Remove-Item -Path $_.FullName }
 Set-Location "./Dependencies/"
 Remove-Item -Path "./arm","./Win32","./x86" –recurse -ErrorAction SilentlyContinue
