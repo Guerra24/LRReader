b7bb93c132bf629305a40b11bc76e9fc134af5b8
Guerra24 <lili@guerra24.net>
2025-11-16 19:57:21 -0600

    Fix background on win10

--- LRReader.UWP/Views/Root.xaml.cs
+++ LRReader.UWP/Views/Root.xaml.cs
@@ -7,7 +7,7 @@ using Windows.UI.Xaml.Controls;
 
 namespace LRReader.UWP.Views
 {
-	public sealed partial class Root : UserControl
+	public sealed partial class Root : Page
 	{
 		public Root()
 		{
